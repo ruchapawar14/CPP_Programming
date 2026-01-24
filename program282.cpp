1c07a37f45d832179d8e2e32072e1a1fa550e455
Rucha Pawar <ruchap2070@gmail.com>
2026-01-24 18:28:38 +0530

    Add files via upload

---
+++ program282.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    int iCnt = 0;
+
+   for(iCnt  = 0; iCnt <= 4; iCnt++)
+    {
+        cout<<"Jay Ganesh...\n";
+    }
+}
+int main()
+{
+    Display();
+    
+    return 0; 
+}
